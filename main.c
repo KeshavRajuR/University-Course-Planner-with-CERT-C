3315dfff5ba20a895d11100e6a46a6b28ddae432
Keshav Raju <44800315+KeshavRajuR@users.noreply.github.com>
2020-04-28 17:26:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 #include "header1.h"
 #include "header2.h"
 
@@ -24,21 +25,21 @@ int chkAccount(char *username, char *passwd)
     char buffer[bufferLength];
 
     check = fopen("creds.txt", "r");
-    
+
     //DCL09-A
-    if(check == NULL)
+    if (check == NULL)
     {
         fprintf(stderr, "PROGRAM RAN INTO ERROR \nERROR VALUE : %d \nERROR MESSAGE : %s\n", errno, strerror(errno));
-        
+
         return -1;
     }
     else
     {
-        // FIO37-C  STR31-C 
+        // FIO37-C  STR31-C
         while (fgets(buffer, bufferLength, check))
         {
             result = strcmp(login_creds, buffer);
-            if(result == 0)
+            if (result == 0)
             {
                 return result;
             }
@@ -86,7 +87,7 @@ int registerUser()
     int registerCount = 5;
     printf("Let's create your login credentials. Rules for username and password:\n");
     printf("1) Username cannot be longer than 10 characters\n2) Password cannot be longer than 20 characetersr\n3) No spaces in the username or password\n");
-	
+
     while (registerCount != 0)
     {
         printf("Enter your username: ");
@@ -102,7 +103,7 @@ int registerUser()
             char user[30];
             strcat(user, username);
             strcat(user, ":");
-            strcat(user,passwd);
+            strcat(user, passwd);
             strcat(user, "");
             newUser = fopen("creds.txt", "a");
             fprintf(newUser, "%s\n", user);
@@ -153,9 +154,86 @@ int login()
 
         --loginCount;
     }
-    if(loginCount == 0)
+    if (loginCount == 0)
     {
         printf("Max limit reached!! Try again later.\n");
     }
     exit(0);
+}
+
+int studentInfo()
+{
+    static strings ok_chars_name[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-.@";
+    static strings ok_chars_srn[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-.@";
+
+    int flag = 0;
+    strings SRN[14], name[20];
+    strings ans[3];
+
+    //Getting some information about the user.
+    //The number before the s is to make sure that scanf will only read that many characters.
+    //This helps us avoid stack smashing
+    //MEM05-A
+    printf("Enter your Full Name: ");
+    scanf(" %[^\n]s ", name);
+
+    printf("Enter your SRN: ");
+    scanf("%14s", SRN);
+
+    strings *namePointer = name;
+    strings *srnPointer = SRN;
+
+    const strings *end_name = name + strlen(name);
+    const strings *end_srn = SRN + strlen(SRN);
+
+    for (namePointer += strspn(namePointer, ok_chars_name); namePointer != end_name; namePointer += strspn(namePointer, ok_chars_name))
+    {
+        *namePointer = '_';
+    }
+
+    for (srnPointer += strspn(srnPointer, ok_chars_srn); srnPointer != end_srn; srnPointer += strspn(srnPointer, ok_chars_srn))
+    {
+        *srnPointer = '_';
+    }
+
+    for (int i = 0; i < strlen(name); i++)
+    {
+        if (name[i] == '_')
+        {
+            printf("Looks like you have entered invalid characters. You can have only letters in your name.\n");
+            i = strlen(name);
+            flag = 1;
+            sleep(5);
+        }
+    }
+
+    for (int i = 0; i < strlen(SRN); i++)
+    {
+        if (SRN[i] == '_')
+        {
+            printf("Looks like you have entered invalid characters. You have to follow the format of the college.\n");
+            i = strlen(SRN);
+            flag = 1;
+            sleep(5);
+        }
+    }
+
+    if(flag == 0)
+    {
+        printf("Your information will be printed here, make sure it is correct.\n");
+        printf("Name: %s\tSRN: %s\n", name, SRN);
+        printf("\nIs everything correct?\n[yes/no]: ");
+        scanf("%3s", ans);
+
+        if (!strcmp(ans, "yes"))
+        {
+            flag = 0;
+        }
+        else
+        {
+            flag = 1;
+        }
+    }
+
+    return flag;
 }
